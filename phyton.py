ec4ebccd06c39eadae2197155f02225607b83c89
ramanamurthy123456 <147133338+ramanamurthy123456@users.noreply.github.com>
2023-10-13 15:50:11 +0530

    Update phyton.py

--- phyton.py
+++ phyton.py
@@ -1,1 +1,10 @@
-print("Welcome to amzuit 1")
+                                             
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'welcome to amzu version1'
+
+app.run(host='0.0.0.0', port=8080)
